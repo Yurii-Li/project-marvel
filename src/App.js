d5b6e55db0ddec1a9008124505ca4e1508568a76
man1fest000 <liubuskiy.yuri@gmail.com>
2022-11-02 15:14:01 +0200

    progect2-marvel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import {AppHeader, CharInfo, CharList, RandomChar} from "./components";
 
-import './App.css';
+import decoration from './resources/img/vision.png'
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <RandomChar/>
+                    <div className="char__content">
+                        <CharList/>
+                        <CharInfo/>
+                    </div>
+                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                </main>
+            </div>
+        </div>
+    );
 }
 
 export default App;
