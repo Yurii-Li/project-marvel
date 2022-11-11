b263603cad9854303227c394a246831891be4f5c
man1fest000 <liubuskiy.yuri@gmail.com>
2022-11-11 21:31:31 +0200

    service getAllCharacters, getCharacter

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,6 @@
 class MarvelService {
-    _apiBase = 'https://gateway.marvel.com:443/v1/public';
-    _apiKey = 'apikey=5210b14785e6643e770990b67a77ccaa';
+    _apiBase = "https://gateway.marvel.com:443/v1/public";
+    _apiKey = "apikey=5210b14785e6643e770990b67a77ccaa";
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -8,16 +8,15 @@ class MarvelService {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
         return await res.json();
-    }
+    };
 
     getAllCharacters = async () => {
         return await this.getResource(`${this._apiBase}/characters?limit=9&${this._apiKey}`);
-    }
+    };
 
     getCharacter = async (id) => {
         return await this.getResource(`${this._apiBase}/characters/${id}?${this._apiKey}`);
-    }
-
+    };
 }
 
 export { MarvelService };
